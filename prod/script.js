6d10ff6bc698b3f906f475dae9cbd4285d0de8ff
Sandro Pereira <sandrodcpereira@gmail.com>
2019-11-25 06:41:18 +0000

    Responsive menu!

--- prod/script.js
+++ prod/script.js
@@ -1,28 +1,41 @@
 // layout component loader
 
 $(function(){
-  $("header").load("layout/header.html"); 
-  $("footer").load("layout/footer.html"); 
-  $("nav").load("layout/nav.html"); 
+	$("header").load("layout/header.html"); 
+	$("footer").load("layout/footer.html"); 
+	$("nav").load("layout/nav.html"); 
 });
 
-// load frames on click
 
-$(function(){
-    var current = location.pathname;
-    $('nav a').each(function(){
-        var $this = $(this);
-        // if the current path is like this link, make it active
-        if($this.attr('href').indexOf(current) !== -1){
-            $this.addClass('active');
-        }
-    })
-})
+// open menu mobile
+
+$(document).on('click', '#menu-toggle, :not(nav)', function() {
+	$('nav').toggleClass("open");
+	console.log('wtf man');
+});
+
+// active menu item
+
+/* $(function(){
+		var current = location.pathname;
+		$('nav a').each(function(){
+				var $this = $(this);
+				// if the current path is like this link, make it active
+				if($this.attr('href').indexOf(current) !== -1){
+						$this.addClass('active');
+				}
+		})
+}) */
+
+
+
 
 $(function(){
 	var pathname = window.location.pathname; // Returns path only (/path/example.html)
 	console.log(pathname, "Hello, world!");
 });
+
+
 
 // video play on hover
 
